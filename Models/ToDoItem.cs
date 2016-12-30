e8da2a44cd6bcfc67c58e0ccf2d3a48b6cf8177c
jayDeOps <jasmin.mehta@yahoo.com>
2016-12-30 10:39:12 -0500

    sqlite database, dbcontext

--- Models/ToDoItem.cs
+++ Models/ToDoItem.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoAPI.Models
 {
+	[Table("todoItem")]
 	public class ToDoItem
 	{
+		[Key]
 		public string Key { get; set; }
 
 		public string Name { get; set; }
---
+++ todoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace .netCore_todoAPI_ef
+{
+    public partial class todoContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlite(@"Data Source=todo.db;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
